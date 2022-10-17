81cc26762e717b9eb9068b408c1540cf968953ae
Parikshit Sathye <psathye@gmail.com>
2022-10-17 18:49:52 +0530

    commiting changes to test case file to resolve issue2

--- src/test/java/testCases/GitTestCase.java
+++ src/test/java/testCases/GitTestCase.java
@@ -22,16 +22,18 @@ public class GitTestCase {
 		
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 				
 	}
 	
 	@Test
 	public void doLogin() throws InterruptedException {
 		
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 		driver.get("http://gmail.com");
-		driver.findElement(By.id("identifierId")).sendKeys("trainer@way2automation.com");
-		//Thread.sleep(2000);
+		driver.findElement(By.id("identifierId")).sendKeys("java@way2automation.com");
+		
 		driver.findElement(By.xpath("//*[@id=\"identifierNext\"]/div/button/span")).click();
 		
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"password\"]/div[1]/div/div[1]/input"))).sendKeys("sdfsdfsdf");
